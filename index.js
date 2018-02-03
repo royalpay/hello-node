cfc3defcedfcf2a82afef52fb455c4ac214cdc59
royalpay <bruceLi@royalpay.com.au>
2018-02-03 21:33:37 +0800

    rename app.js to index.js and change port to 9000

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app =express();
 
+var port = 9000;
+
 app.get('/',function(req,res){
    res.send("Hello World!");
 });
 
-app.listen(4000);
+app.listen(port);
+
+console.log("Running on http://localhost:" + port);
